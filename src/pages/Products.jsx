5dd4943a99271a39512d29f2bfeb6ee23a639a51
kenizaya <harveylstark@gmail.com>
2022-07-16 01:32:23 +0530

    Add sweatshirts to products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { sweatshirts } from '../data/sweatshirts'
+import styles from '../styles/Products.module.css'
 
 const Products = () => {
-  return <div>Products</div>
+  return (
+    <div className={styles.productsContainer}>
+      {sweatshirts.map((item) => {
+        return (
+          <div key={item.id} className={styles.productCard}>
+            <img src={item.src} alt={item.name} />
+            <p>{item.name}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Products
