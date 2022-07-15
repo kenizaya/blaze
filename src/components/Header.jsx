48c1b22e3f96e05e4f217e2570d51781436b0aad
kenizaya <harveylstark@gmail.com>
2022-07-16 01:55:35 +0530

    Add cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { BsBag, BsBagFill } from 'react-icons/bs'
 import styles from '../styles/Header.module.css'
 
 const Header = () => {
@@ -7,6 +8,7 @@ const Header = () => {
     <header className={styles.header}>
       <h1>Blaze</h1>
       <Navbar />
+      <BsBag className={styles.cartIcon} />
     </header>
   )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { sweatshirts } from '../data/sweatshirts'
 import styles from '../styles/Products.module.css'
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
 
 const Products = () => {
   return (
@@ -10,6 +11,13 @@ const Products = () => {
           <div key={item.id} className={styles.productCard}>
             <img src={item.src} alt={item.name} />
             <p>{item.name}</p>
+            <div className={styles.priceAndIcon}>
+              <span className={styles.price}>${item.price}</span>
+              <span>
+                <BsChevronLeft />
+                <BsChevronRight />
+              </span>
+            </div>
           </div>
         )
       })}
