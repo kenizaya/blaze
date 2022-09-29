042adfdcdd384b0a9bba25c98faaf56375308cc6
kenizaya <harveylstark@gmail.com>
2022-09-29 10:28:51 +0530

    Fix cart on checkout

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -31,7 +31,6 @@ const Cart = () => {
               <button
                 className={styles.checkoutBtn}
                 onClick={() => {
-                  clearCart()
                   navigate('/')
                 }}
               >
