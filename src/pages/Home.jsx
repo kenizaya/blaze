3ba31f749d38a2d3ba1e1781ec2bf697c62f5d4d
kenizaya <harveylstark@gmail.com>
2022-10-17 01:47:13 +0530

    Update Home page UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import styles from '../styles/Home.module.css'
 
 const Home = () => {
-  return <Hero />
+  return (
+    <main className={styles.main}>
+      <Hero />
+    </main>
+  )
 }
 
 export default Home
