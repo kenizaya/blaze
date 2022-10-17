ba7fcb24e11f5c68947e1aba072dfa5c7f1744d1
kenizaya <harveylstark@gmail.com>
2022-10-18 00:37:11 +0530

    Add Ratings

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
+import styles from '../styles/Ratings.module.css'
 
-const Ratings = () => {
-  return <div>Ratings</div>
+const Ratings = ({ ratings, reviews }) => {
+  console.log(ratings, reviews)
+  const stars = Array.from({ length: 5 }, (_, index) => {
+    return (
+      <span key={index}>
+        {ratings >= index + 1 ? (
+          <BsStarFill />
+        ) : ratings >= index - 0.5 ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    )
+  })
+
+  return (
+    <div className={styles['ratings-wrapper']}>
+      <div className={styles.ratings}>{stars}</div>
+      <p className={styles.reviews}>({reviews} reviews)</p>
+    </div>
+  )
 }
 
 export default Ratings
