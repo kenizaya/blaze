34202e72ac42374f00b540781e22b0eeef7d3a8b
kenizaya <harveylstark@gmail.com>
2022-10-18 00:00:45 +0530

    Add ProductImages

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { useState } from 'react'
+import cn from 'classnames'
+import styles from '../styles/ProductImages.module.css'
 
-const ProductImages = () => {
-  return <div>ProductImages</div>
+const ProductImages = ({ images }) => {
+  const [mainImage, setMainImage] = useState(images[0])
+  console.log(mainImage)
+  return (
+    <section>
+      <img
+        src={mainImage}
+        alt='main dress'
+        className={cn(styles.main, styles.img)}
+      />
+      <div className={styles.gallery}>
+        {images.map((image, index) => {
+          return (
+            <img
+              src={image}
+              alt='placeholder'
+              key={index}
+              className={
+                image === mainImage ? cn(styles.img, styles.active) : styles.img
+              }
+              onClick={() => setMainImage(image)}
+            />
+          )
+        })}
+      </div>
+    </section>
+  )
 }
 
 export default ProductImages
