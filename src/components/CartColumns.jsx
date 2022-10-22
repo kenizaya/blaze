1626a0dcfd24c2787b1fc7eafe6fc4921079167d
kenizaya <harveylstark@gmail.com>
2022-10-22 13:18:41 +0530

    Implement CartColumns

--- src/components/CartColumns.jsx
+++ src/components/CartColumns.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import styles from '../styles/CartColumns.module.css'
 
 const CartColumns = () => {
-  return <div>CartColumns</div>
+  return (
+    <div className={styles.div}>
+      <div className={styles.content}>
+        <h5>Item</h5>
+        <h5>Price</h5>
+        <h5>Quantity</h5>
+        <h5>Subtotal</h5>
+      </div>
+      <hr />
+    </div>
+  )
 }
 
 export default CartColumns
