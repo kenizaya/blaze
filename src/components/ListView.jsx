70190388d4f9380c705a0c5592d9711a58e09330
kenizaya <harveylstark@gmail.com>
2022-10-23 13:07:03 +0530

    Add ratings and add to cart button in ListView

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -2,18 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from '../styles/ListView.module.css'
 import { formatPrice } from '../utils/helpers'
+import AddToCartButton from './AddToCartButton'
+import Ratings from './Ratings'
 
 const ListView = ({ products }) => {
   return (
     <section className={styles['products-container']}>
       {products.map((product) => {
-        const { name, image, price, id, description } = product
+        const { name, image, price, id, description, ratings, reviews } =
+          product
         return (
           <div key={id} className={styles['product-wrapper']}>
             <img src={image} alt={name} />
             <div className={styles['info-wrapper']}>
               <h4 className={styles.name}>{name}</h4>
-              <h5 className={styles.price}>{formatPrice(price)}</h5>
+              <Ratings ratings={ratings} reviews={reviews} />
+              <div className={styles['price-cart-wrapper']}>
+                <h5 className={styles.price}>{formatPrice(price)}</h5>
+                <AddToCartButton product={product} />
+              </div>
+
               <p className={styles.description}>
                 {description.substring(0, 250)}...
               </p>
