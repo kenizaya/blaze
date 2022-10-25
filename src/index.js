2825267564713c1e6a957987d635d87da010d8c2
kenizaya <harveylstark@gmail.com>
2022-10-25 11:45:12 +0530

    Setup Auth0Provider

--- src/index.js
+++ src/index.js
@@ -4,20 +4,28 @@ import { CartProvider } from './context/cart_context'
 import { FilterProvider } from './context/filter_context'
 import { ProductsProvider } from './context/products_context'
 import { WishlistProvider } from './context/wishlist_context'
+import { Auth0Provider } from '@auth0/auth0-react'
 import './index.css'
 import RouteSwitch from './RouteSwitch'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <WishlistProvider>
-            <RouteSwitch />
-          </WishlistProvider>
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <WishlistProvider>
+              <RouteSwitch />
+            </WishlistProvider>
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </Auth0Provider>
   </React.StrictMode>
 )
